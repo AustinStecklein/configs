cf909fa8a07d691e52646f5b6571b4ee51e35e25
austin <astecklein@gmail.com>
2025-10-07 20:55:02 -0700

    little bit of tweaking

--- nvim/init.lua
+++ nvim/init.lua
@@ -43,7 +43,6 @@ require("lazy").setup({
         {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
         {"neovim/nvim-lspconfig"},
         {"ibhagwan/fzf-lua"},
-        {"AustinStecklein/neo-find"},
         {'tpope/vim-fugitive'},
         {'tommcdo/vim-fubitive'},
         {'tpope/vim-rhubarb'},
@@ -72,7 +71,7 @@ vim.cmd.source(vimrc)
 vim.o.pumheight = 5
 require'nvim-treesitter.configs'.setup {
         -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "asm", "bash", "odin", "rust", "python"},
+        ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "asm", "bash", "odin", "rust", "python"},
         auto_install = true,
         highlight = {
             enable = true,
@@ -81,9 +80,6 @@ require'nvim-treesitter.configs'.setup {
 -- key mapping for delete without saving
 vim.keymap.set("n", "<leader>dd", '"_dd')
 vim.keymap.set("v", "<leader>d", '"_d')
-
--- key mapping for replace with default register
-vim.keymap.set("v", "<leader>r", '"_dP')
 
 -- FzfLua keybindings
 require("fzf-lua").setup({
@@ -98,21 +94,14 @@ local fzflua = require("fzf-lua")
 -- I can't believe I haven't been using this. This is a great plugin
 vim.keymap.set("n", "<Leader>f", function() fzflua.files() end)
 vim.keymap.set("n", "<Leader>g", function() fzflua.live_grep() end)
+vim.keymap.set("n", "<Leader>G", function() fzflua.live_grep_resume() end)
 vim.keymap.set("n", "<Leader>c", function() fzflua.git_commits() end)
 vim.keymap.set("n", "<Leader>s", function() fzflua.git_status() end)
 vim.keymap.set("n", "<Leader>b", function() fzflua.git_blame() end)
 vim.keymap.set("n", "<Leader>w", function() fzflua.grep_cword() end)
 vim.keymap.set("n", "<Leader>W", function() fzflua.grep_cWORD() end)
-
-local neo_find = require("neo-find")
--- the ! -path's and the ! -name's are here to filter dirs that I don't want to see to clean up the search
-local ignore_args  = " ! -path '*/venv/*' ! -name 'venv' ! -path '*/.*/*' ! -name '.*' "
-vim.keymap.set("n", "<Leader>df", function() neo_find.find_dir(ignore_args) end)
--- this command will not have the filter applied in case that I need to find dirs in the filtered files
-vim.keymap.set("n", "<Leader>hf", function() neo_find.find_dir() end)
--- This is the bread and butter commands
-vim.keymap.set("n", "<Leader>F", function() neo_find.find_dir_and_search(ignore_args, fzflua.files) end)
-vim.keymap.set("n", "<Leader>HF", function() neo_find.find_dir_and_search(ignore_args, fzflua.live_grep) end)
+vim.keymap.set("n", "<Leader>qg", function() fzflua.lgrep_quickfix() end)
+vim.keymap.set("n", "<Leader>r", function() fzflua.resume() end)
 
 require("oil").setup({
     view_options = {
@@ -122,11 +111,6 @@ require("oil").setup({
 require'lspconfig'.pyright.setup{}
 require'lspconfig'.clangd.setup{}
 
--- better movement through quickfix lists
-vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
-vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
-
-vim.keymap.set("t", "<C-q>", "<C-\\><C-n>")
 vim.diagnostic.config({virtual_text = true})
 vim.keymap.set("n","<Leader>e", function()
     vim.diagnostic.enable(not vim.diagnostic.is_enabled())
